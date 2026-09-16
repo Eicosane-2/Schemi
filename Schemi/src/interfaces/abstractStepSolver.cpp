1cdcee24b6889d8e72cde99ebd86878ab3306e64
Maxim Boldyrev <eicosane@ya.ru>
2026-09-17 01:06:19 +0900

    Added cutting of small floating point values.

--- Schemi/src/interfaces/abstractStepSolver.cpp
+++ Schemi/src/interfaces/abstractStepSolver.cpp
@@ -13,6 +13,24 @@
 #include "thirdOrderStepSolver.hpp"
 #include "thirdOrderStepSolverCada.hpp"
 
+void schemi::abstractStepSolver::normalize(std::valarray<scalar> & res) noexcept
+{
+	std::replace_if(std::begin(res), std::end(res), [](const auto & i)
+	{
+		return std::abs(i) < std::numeric_limits<scalar>::epsilon();
+	}, 0);
+}
+
+void schemi::abstractStepSolver::normalize(std::valarray<vector> & res) noexcept
+{
+	for (auto & r_i : res)
+	{
+		for (std::size_t j = 0; j < vector::vsize; ++j)
+			if (std::abs(r_i()[j]) < std::numeric_limits<scalar>::epsilon())
+				r_i.wr()[j] = 0;
+	}
+}
+
 schemi::abstractStepSolver::abstractStepSolver(
 		homogeneousPhase<cubicCell> & gasPhase_in,
 		const abstractLimiter & limiter_in,
